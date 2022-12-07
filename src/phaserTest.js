c315800011dc6fb33a9ed67b20140a35ef8c6398
Cardioth <flashkid@hotmail.com>
2022-12-07 17:08:06 +1000

    added all parts

--- src/phaserTest.js
+++ src/phaserTest.js
@@ -23,49 +23,102 @@ var workshopInterfaceX = 600;
 var workshopInterfaceY = 400;
 var totalPartsListWidth;
 
-var rootNode;
 var allNodes = [];
+var hatchPlaced = false;
 var allPingGraphics = [];
 
-//Hull widths cause I got lazy
-var BHW = 343;
-var FHW = 171;
-var MHW = 252;
+//Hull widths
+var BHW = 342;
+var FHW = 170;
+var MHW = 251;
 
 var nodeStructure = {
     rootNode:{
-        x: -22.75, y: -102.5, subNodes: [
+        name:'rootNode', x: -22.75, y: -102.5, subNodes: [
             {part:'middle_hull',x:0,y:0,engaged: false, connectedWith: 'M'}, 
             {part:'front_hull',x:0,y:0,engaged: false, connectedWith: 'M'}, 
             {part:'back_hull',x:0,y:0,engaged: false, connectedWith: 'M'},
         ],
     },
     front_hull:{
-        subNodes: [
+        name:'front_hull',subNodes: [
             {part:'middle_hull',x:(MHW+FHW)/2,y:0,engaged: false, connectedWith: 'A'},
-            {part:'window',x:0,y:0,engaged: false, connectedWith: 'B'},
+            {part:'window',x:30,y:0,engaged: false, connectedWith: 'F'},
+            {part:'flair1',x:30,y:-40,engaged: false, connectedWith: 'F'},
+            {part:'flair2',x:30,y:-40,engaged: false, connectedWith: 'F'},
+            {part:'frontHardpoint',x:-50,y:85,engaged: false, connectedWith: 'G'},
         ], 
     },
     middle_hull:{
-        subNodes: [
+        name:'middle_hull',subNodes: [
             {part:'front_hull',x:(FHW+MHW)/2,y:0,engaged: false, flipped:true, connectedWith: 'A'},
             {part:'front_hull',x:-(FHW+MHW)/2,y:0,engaged: false, connectedWith: 'B'},
             {part:'middle_hull',x:MHW,y:0,engaged: false, connectedWith: 'A'},
             {part:'middle_hull',x:-MHW,y:0,engaged: false, connectedWith: 'B'},
             {part:'back_hull',x:(BHW+MHW)/2,y:0,engaged: false, connectedWith: 'A'},
             {part:'back_hull',x:-(BHW+MHW)/2,y:0,engaged: false, flipped:true, connectedWith: 'B'},
+            {part:'flipper',x:-80,y:70,engaged: false, connectedWith: 'C'},
+            {part:'flipper',x:80,y:70,engaged: false, connectedWith: 'C'},
+            {part:'top_hatch',x:-80,y:-155,engaged: false, connectedWith: 'D'},
+            {part:'top_plate',x:-80,y:-80,engaged: false, connectedWith: 'D'},
+            {part:'gun_assembly',x:-80,y:-135,engaged: false, connectedWith: 'D'},
+            {part:'top_hatch',x:80,y:-155,engaged: false, connectedWith: 'E'},
+            {part:'top_plate',x:80,y:-80,engaged: false, connectedWith: 'E'},
+            {part:'gun_assembly',x:80,y:-135,engaged: false, connectedWith: 'E'},
+            {part:'window',x:0,y:0,engaged: false, connectedWith: 'F'},
+            {part:'flair1',x:0,y:-40,engaged: false, connectedWith: 'F'},
+            {part:'flair2',x:0,y:-40,engaged: false, connectedWith: 'F'},
         ], 
     },
     back_hull:{
-        subNodes: [
-            {part:'middle_hull',x:-(MHW+BHW)/2,y:0,engaged: false, connectedWith: 'A'},
-            {part:'window',x:-50,y:0,engaged: false, connectedWith: 'B'},
+        name:'back_hull',subNodes: [
+            {part:'back_armor',x:-120,y:110,engaged: false, connectedWith: 'G'},
+            {part:'top_plate',x:-120,y:-80,engaged: false, connectedWith: 'E'},
+            {part:'gun_assembly',x:-120,y:-135,engaged: false, connectedWith: 'E'},
+            {part:'middle_hull',x:-(MHW+BHW)/2,y:0,engaged: false, connectedWith: 'B'},
+            {part:'window',x:-50,y:0,engaged: false, connectedWith: 'F'},
+            {part:'flair1',x:-50,y:-40,engaged: false, connectedWith: 'F'},
+            {part:'flair2',x:-50,y:-40,engaged: false, connectedWith: 'F'},
+            {part:'propeller',x:200,y:-20,engaged: false, connectedWith: 'D'},
         ], 
     },
     window:{
-        subNodes: [], 
-    }
+        name:'window',subNodes: [], 
+    },
+    propeller:{
+        name:'propeller',subNodes: [], 
+    },
+    flipper:{
+        name:'flipper',subNodes: [], 
+    },
+    top_hatch:{
+        name:'top_hatch',subNodes: [], 
+    },
+    top_plate:{
+        name:'top_plate',subNodes: [], 
+    },
+    gun_assembly:{
+        name:'gun_assembly',subNodes: [], 
+    },
+    back_armor:{
+        name:'back_armor',subNodes: [], 
+    },
+    frontHardpoint:{
+        name:'frontHardpoint',subNodes: [
+            {part:'miningLaser',x:-60,y:40,engaged: false, connectedWith: 'K'},
+        ], 
+    },
+    flair1:{
+        name:'flair1',subNodes: [], 
+    },
+    flair2:{
+        name:'flair2',subNodes: [], 
+    },
+    miningLaser:{
+        name:'miningLaser',subNodes: [], 
+    },
 }
+
 nodeStructure = JSON.stringify(nodeStructure);
 
 function preload()
@@ -158,18 +211,7 @@ function create ()
     for(var parts of allParts){
         createPart(parts, true);
     }
-
-    //How to handle adding a group of parts to shop
-    var gun_turret = this.add.image(0,-60,'submarine','gun_turret.png').setInteractive();
-    var gun_base = this.add.image(0,0,'submarine','gun_base.png').setInteractive();
-    var gun_assembly = this.add.container(0,0,[
-        gun_turret,
-        gun_base,
-    ]);
-    gun_assembly.setSize(gun_base.width+50,gun_turret.height+50);
-    gun_assembly.setInteractive();
-    gun_assembly.partType = 'gun_assembly';
-    partsList.push(gun_assembly);
+    createPart([['gun_turret','gun_base'],true,'gun_assembly']);
 
     //Add all the parts to the screen interface
     for(var i=0;i<partsList.length;i++){
@@ -183,12 +225,11 @@ function create ()
     //Building Sub Screen
 
     //Create Root Node
-    rootNode = this.add.container();
+    var rootNode = this.add.container();
     var newStructure = JSON.parse(nodeStructure);
     rootNode = Object.assign(rootNode, newStructure.rootNode);
-    rootNode.scale = 0.4;
+    rootNode.scale = 0.3;
     allNodes.push(rootNode);
-    rootNode.add(pingGraphic(0,0));
 
     buildInterface = this.add.container();
     buildInterface.add(rootNode);
@@ -266,12 +307,28 @@ function create ()
 }
 
 function createPart(partName, addingToShop){
-    var part = scene.add.image(0,0,'submarine',partName+'.png').setInteractive(); 
-    part.partType = partName;
-    if(addingToShop == true){
-        partsList.push(part);
+    if(typeof partName === 'string'){
+        var part = scene.add.image(0,0,'submarine',partName+'.png').setInteractive(); 
+        part.partType = partName;
+        if(addingToShop == true){
+            partsList.push(part);
+        }
+        return part;
+    } else {
+        var assembly = scene.add.container(0,0);
+        for(let parts of partName[0]){
+            let part = scene.add.image(0,0,'submarine',parts+'.png');
+            assembly.add(part);
+        }
+        assembly.setSize(assembly.getBounds().width,assembly.getBounds().height); //Why isn't this just done automatically Phaser?
+        assembly.setInteractive();
+        assembly.partType = partName[2];
+        if(partName[1] == true){
+            partsList.push(assembly);
+        }
+        return assembly;
     }
-    return part;
+
 }
 
 function sumOfPreviousPartsWidth(i,parts, margin){
@@ -331,22 +388,41 @@ function addPartToBuildInterface(part,x,y,node,partType,flipped,originNode){
         partContainer.scaleX = -1;
     }
     var newStructure = JSON.parse(nodeStructure);
+    if(Array.isArray(partType)){
+        partType = partType[2];
+    }
     partContainer = Object.assign(partContainer, newStructure[partType]);
+    
+
+
     //Preventing reverse connections
-    if(partType == 'middle_hull' || partType == 'back_hull' || partType == 'front_hull'){
+    if(partType == 'middle_hull'){
         if(originNode.connectedWith == 'A'){
             partContainer.subNodes = partContainer.subNodes.filter(function(obj){
-                return obj.connectedWith === 'A';
+                return obj.connectedWith !== 'B';
             });
         }
         if(originNode.connectedWith == 'B'){
             partContainer.subNodes = partContainer.subNodes.filter(function(obj){
-                return obj.connectedWith === 'B';
+                return obj.connectedWith !== 'A';
             });
         }
     }
-   
+    if(partType == 'back_hull' || partType == 'front_hull'){
+        if(originNode.connectedWith == 'A' || originNode.connectedWith == 'B'){
+            partContainer.subNodes = partContainer.subNodes.filter(function(obj){
+                return obj.connectedWith !== 'B' && obj.connectedWith !== 'A';
+            });
+        }
+    }
+    //Only one hatch
+    if(partType == 'top_hatch'){
+        hatchPlaced = true;
+    }
     setTintPart(part,0x3BAA59);
+    if(partType == 'flair1' || partType == 'flair2'){
+        setTintPart(part,0x3BFF59);
+    }
     partContainer.add(part);
     node.add(partContainer);
     allNodes.push(partContainer);
@@ -358,7 +434,11 @@ function addPartToBuild(partType){
     for(var nodes of allNodes){
         for(var subNodes of nodes.subNodes){
             if(subNodes.part == partType && subNodes.engaged == false){
-                foundNodes.push({PartType:partType,node:nodes,x:subNodes.x, y:subNodes.y, subNodes:subNodes, connectionType:subNodes.connectedWith, flipped:subNodes.flipped});
+                //There's some bandaids here, sorry
+                if(partType == 'top_hatch' && hatchPlaced){return;}//quick and dirty preventing more than one hatch
+                let  newPartType = partType; //I'm sorry
+                if(partType == 'gun_assembly'){newPartType = [['gun_turret','gun_base'],false,'gun_assembly']}; //quick and dirty way of adding the turret           
+                foundNodes.push({PartType:newPartType,node:nodes,x:subNodes.x, y:subNodes.y, subNodes:subNodes, connectionType:subNodes.connectedWith, flipped:subNodes.flipped});
             }
         }
     }
